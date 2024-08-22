760157ca4a36c21098e248f0be8f7208a5ea0b8e
madortill <mador.till@gmail.com>
2024-08-22 10:08:51 +0300

    sd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import index from './index'
 import './assets/main.css'
 
-// const app = createApp(App)
-createApp(App).use(index).mount('#app')
+const app = createApp(App)
 app.mount('#app')
