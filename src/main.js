639679dc4e6af7ba12b30a8466620cdab58d0851
madortill <mador.till@gmail.com>
2024-08-22 10:05:56 +0300

    dd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import index from './index'
 import './assets/main.css'
 
 // const app = createApp(App)
-createApp(App).use(router).mount('#app')
+createApp(App).use(index).mount('#app')
 app.mount('#app')
