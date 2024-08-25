1f11d25e003a9d60681f9626dcf67e677e04bc0d
madortill <mador.till@gmail.com>
2024-08-25 16:53:25 +0300

    sdsd

--- src/components/Prep.vue
+++ src/components/Prep.vue
@@ -30,7 +30,7 @@
 <script>
 // import GoodWatch from './components/GoodWatch.vue';
 // import GoodWatching from './goodWatching.vue';
-import GoodWatching from './GoodWatching.vue';
+import GoodWatching from '@/GoodWatching.vue';
 
 export default {
     props: ["chapter"],
